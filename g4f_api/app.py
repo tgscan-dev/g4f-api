9436022779b961af4b9cf3b2169f31e7f19569ec
tgscan-dev <tgscan.xyz@outlook.com>
2023-10-21 14:46:24 +0800

    Add auto proxy

--- g4f_api/app.py
+++ g4f_api/app.py
@@ -1,6 +1,7 @@
 import json
 import random
 import string
+import threading
 import time
 from typing import Any
 
@@ -8,7 +9,9 @@ import g4f
 from fastapi import FastAPI, Request
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.responses import StreamingResponse
+from fp.fp import FreeProxy
 from g4f import ChatCompletion
+from g4f.Provider import GptGo
 from loguru import logger
 from starlette.middleware.cors import CORSMiddleware
 
@@ -23,6 +26,23 @@ app.add_middleware(
 )
 
 
+# thread local proxy variable
+thread_local = threading.local()
+thread_local.proxy = None
+free_proxy = FreeProxy(timeout=1)
+
+
+def get_proxy(is_working: bool = True):
+    if is_working:
+        if getattr(thread_local, "proxy") is None:
+            setattr(thread_local, "proxy", free_proxy.get())
+        else:
+            return thread_local.proxy
+    else:
+        setattr(thread_local, "proxy", free_proxy.get())
+        return thread_local.proxy
+
+
 @app.post("/chat/completions")
 @app.post("/v1/chat/completions")
 async def chat_completions(request: Request):
@@ -34,22 +54,17 @@ async def chat_completions(request: Request):
     top_p = req_data.get("top_p", 1.0)
     max_tokens = req_data.get("max_tokens", 4096)
 
-    logger.info(f"chat_completions: {req_data}, stream: {stream}, model: {model}, messages: {messages}, temperature: {temperature}, top_p: {top_p}, max_tokens: {max_tokens}")
-
-    response = ChatCompletion.create(
-        model=model,
-        stream=stream,
-        messages=messages,
-        temperature=temperature,
-        top_p=top_p,
-        max_tokens=max_tokens,
-        system_prompt="",
+    logger.info(
+        f"chat_completions:  stream: {stream}, model: {model}, temperature: {temperature}, top_p: {top_p}, max_tokens: {max_tokens}"
     )
+
+    response = await gen_resp(max_tokens, messages, model, stream, temperature, top_p)
 
     completion_id = "".join(random.choices(string.ascii_letters + string.digits, k=28))
     completion_timestamp = int(time.time())
 
     if not stream:
+        logger.info(f"chat_completions:  response: {response}")
         return {
             "id": f"chatcmpl-{completion_id}",
             "object": "chat.completion",
@@ -113,6 +128,27 @@ async def chat_completions(request: Request):
     return StreamingResponse(streaming(), media_type="text/event-stream")
 
 
+async def gen_resp(max_tokens, messages, model, stream, temperature, top_p):
+    is_working = True
+    while True:
+        try:
+            response = ChatCompletion.create(
+                model=model,
+                stream=stream,
+                messages=messages,
+                temperature=temperature,
+                top_p=top_p,
+                max_tokens=max_tokens,
+                system_prompt="",
+                provider=GptGo,
+                proxy=get_proxy(is_working),
+            )
+            return response
+        except Exception as e:
+            logger.error(f"gen_resp:  Exception: {e}")
+            is_working = False
+
+
 @app.post("/v1/completions")
 async def completions(request: Request):
     req_data = await request.json()
@@ -148,4 +184,3 @@ async def completions(request: Request):
             "total_tokens": None,
         },
     }
-
